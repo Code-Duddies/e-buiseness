9f8ef9efddf3fdb618cc7d9fe115ce1b1563809f
RaymondHornDev <132532625+RaymondHornDev@users.noreply.github.com>
2024-05-17 20:50:47 -0400

    meh

--- Flask/core.py
+++ Flask/core.py
@@ -1,17 +1,17 @@
 from flask import Flask, redirect, url_for, render_template
-from Logger import LoggerCalls
+#from Logger import LoggerCalls
 
-lc = LoggerCalls()
+#lc = LoggerCalls()
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    lc.debug_call('Landing page called')
+    #lc.debug_call('Landing page called')
     return render_template("index.html")
 
 @app.route("/about_us")
 def about_us():
-    lc.debug_call('About us page called')
+    #lc.debug_call('About us page called')
     return render_template("about-us.html")
 
 if __name__ == "__main__":
