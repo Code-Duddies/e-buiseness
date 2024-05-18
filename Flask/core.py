29c7e43bc3d2004a4d795e0de0c2b6130ff122e6
RaymondHornDev <132532625+RaymondHornDev@users.noreply.github.com>
2024-05-17 20:09:03 -0400

    Update core.py

--- Flask/core.py
+++ Flask/core.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/about_us")
+def about_us():
+    return render_template("about-us.html")
+
 if __name__ == "__main__":
     app.run()
